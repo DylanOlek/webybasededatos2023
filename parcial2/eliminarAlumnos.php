378ed2a8c8d8149e8a50013dd1e63dd289f19cd6
DylanOlek <124410643+DylanOlek@users.noreply.github.com>
2023-10-30 19:28:39 -0700

    Parcial 30/10/23

---
+++ parcial2/eliminarAlumnos.php
@@ -0,0 +1,15 @@
+<?php
+    include 'conexion.php'
+
+    $id = $_GET['id'];
+    
+    $sql = "DELETE FROM alumnos where id =" .$id;
+
+    if($conexion->query($sql) === TRUE){
+        header("Location: consultas.php");
+        $conexion->close();
+        exit;
+    }else{
+        echo "<h2>Ocurrió un error</h2> <p>Error: " .$sql . "<br>" . $conexion->error . "</p>"
+        echo "<h3><a href='consultas.php'>Regresar alumnos</a></h3>";
+    }
